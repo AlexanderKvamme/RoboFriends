c914a499c99cc201de5dfc844c716a50a424d494
Alexander Kvamme <alexanderkvamme@gmail.com>
2021-04-19 11:29:45 +0200

    Stage 2

---
+++ src/Card.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Card = (props) => {
+    const { name, email, id } = props;
+return (
+    <div className="bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5">
+        <img alt='robots' src={`https://robohash.org/${props.name}?200x200`} />
+
+        <div>
+            <h2>{name}</h2>
+            <p>{email}</p>
+            <p>{id}</p>
+        </div>
+    </div>
+);
+}
+
+export default Card;
